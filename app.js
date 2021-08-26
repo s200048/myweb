0ac94230ac4a67beda25b0eee640051a29e398e1
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-08-26 10:31:50 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req,res) => {
+    res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("Server is now running on port 3000.")
+});
