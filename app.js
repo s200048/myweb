2f7e6c89739392da3707d0ffd7d00bb1773e5528
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-08-27 19:38:36 +0800

    20210827 add scss and a little amend of first page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("public"));
 
 
 app.get("/", (req,res) => {
