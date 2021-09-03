2a662a1f892a9cc775ba37d5864b81cccbaad204
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-09-03 18:42:20 +0800

    20210903 add reminding three pages route

--- app.js
+++ app.js
@@ -6,10 +6,29 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-
+// Homepage
 app.get("/", (req,res) => {
     res.render("index");
 });
+
+// About Page
+app.get("/about", (req,res) => {
+    res.render("aboutViews/about");
+})
+
+// Case Page
+app.get("/case", (req,res) => {
+    res.render("caseViews/case");
+})
+
+// Contact Page
+app.get("/contact", (req,res) => {
+    res.render("contactViews/contact");
+})
+
+// app.post("/contact", (req,res)=> {
+//     res.render("contactViews/contact");
+// })
 
 app.listen(3000, () => {
     console.log("Server is now running on port 3000.")
